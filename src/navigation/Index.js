1ca904b17f2e9411ec4c6afb6b683ad6b74d1310
NasrinHaidari <nasrinhaidari786@gmail.com>
2021-02-22 18:22:23 +0430

    import AddNotes screen in Index navigation file

--- src/navigation/Index.js
+++ src/navigation/Index.js
@@ -2,15 +2,20 @@ import { createAppContainer } from 'react-navigation'
 import {createStackNavigator} from 'react-navigation-stack'
 
 import ViewNotes from '../screens/ViewNotes'
+import AddNotes from '../screens/AddNotes'
 
 const StackNavigator = createStackNavigator({
     ViewNotes: {
         screen: ViewNotes
+    },
+    AddNotes: {
+        screen: AddNotes
     }
 },
 {
     initialRouteName: 'ViewNotes',
-    headerMode: 'none'
+    headerMode: 'none',
+    mode: 'modal'
 }
 )
 
