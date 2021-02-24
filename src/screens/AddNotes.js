710e97da2154158d9dab83b61f75fd4575e344e6
NasrinHaidari <nasrinhaidari786@gmail.com>
2021-02-24 17:01:14 +0430

    make for AddNotes screen input field and make button to close the page and make button to save the note on the page

--- src/screens/AddNotes.js
+++ src/screens/AddNotes.js
@@ -1,16 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {StyleSheet, View} from 'react-native'
-import { Text } from 'react-native-paper'
+import { Text, IconButton, TextInput, FAB } from 'react-native-paper'
 import Header from '../component/Header'
 
-function AddNotes() {
+function AddNotes({navigation}) {
+    const [noteTitle, setNoteTitle] = useState('')
+    const [noteDescription, setNoteDescription] = useState('')
+
+// when we press to save button we will navigate back to ViewNote Screen
+    function onSaveNote() {
+        navigation.state.params.addNote({noteTitle, noteValue})
+        navigation.goBack()
+    }
+
+
     return(
         <>
         <Header titleText= 'Add a New Note'/>
+        <IconButton 
+        icon = 'close'
+        size = {25}
+        color = 'white'
+        onPress = {()=>navigation.goBack()}
+        style = {styles.iconButton}
+        />
+
         <View style = {styles.container}>
-            <View style= {styles.titleContainer}>
-                <Text style={styles.title}>Add Notes Model Screen</Text>
-            </View>
+
+            <TextInput
+                label="Add Note Title here"
+                value= {noteTitle}
+                mode='outlined'
+                onChangeText={setNoteTitle}
+                style={styles.title}
+            />
+            <TextInput
+                label="Add Note Description"
+                value= {noteDescription}
+                onChangeText= {setNoteDescription}
+                mode="flat"
+                multiline={true}
+                style={styles.text}
+                scrollEnabled={true}
+                returnKeyLabel= 'done'
+                blurOnSubmit= {true}
+            />
+            <FAB
+                style= {styles.fab}
+                small
+                icon= "check"
+                disabled= {noteTitle == '' ? true : false}
+                onPress= {()=>onSaveNote()}
+            />
         </View>
         </>
     )
@@ -23,13 +64,31 @@ const styles = StyleSheet.create({
         paddingVertical: 20,
         paddingHorizonta:10
     },
+    iconButton: {
+        backgroundColor: '#219653',
+        position: 'absolute',
+        right: 0,
+        top: 4,
+        margin: 10
+    },
     titleContainer: {
         alignItems: 'center',
         justifyContent: 'center',
         flex: 1
     },
     title: {
-        fontSize: 20
+        fontSize: 24,
+        marginBottom: 16
+    },
+    text:{
+        height: 300,
+        fontSize: 16
+    },
+    fab:{
+        position: 'absolute',
+        margin: 20,
+        right: 0,
+        bottom: 0
     }
 })
 
